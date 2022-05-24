7f98e6540c81d6b9658a887e46485829b28f768a
ItzCelyrian <ItzCelyrian@gmail.com>
2022-05-24 11:04:54 +0200

    Updated Strings
    Aufgabe1 hinzugefügt

---
+++ 2xHET/Strings/Aufgabe1/Aufgabe1/Form1.Designer.cs
@@ -0,0 +1,121 @@
+﻿namespace Aufgabe1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Erforderliche Designervariable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Verwendete Ressourcen bereinigen.
+        /// </summary>
+        /// <param name="disposing">True, wenn verwaltete Ressourcen gelöscht werden sollen; andernfalls False.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Vom Windows Form-Designer generierter Code
+
+        /// <summary>
+        /// Erforderliche Methode für die Designerunterstützung.
+        /// Der Inhalt der Methode darf nicht mit dem Code-Editor geändert werden.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(574, 61);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(127, 114);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Generieren";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(574, 181);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(127, 114);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "Leeren";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(574, 305);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(127, 61);
+            this.button3.TabIndex = 2;
+            this.button3.Text = "Beenden";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Location = new System.Drawing.Point(12, 12);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(556, 353);
+            this.richTextBox1.TabIndex = 3;
+            this.richTextBox1.Text = "";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(575, 33);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(126, 22);
+            this.textBox1.TabIndex = 4;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(575, 13);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(96, 16);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "Passwortlänge";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(713, 377);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.richTextBox1);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Passwort Generator";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ 2xHET/Strings/Aufgabe1/Aufgabe1/Form1.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Aufgabe1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string Zeichen = "!§$%&/()=?{[]}'" + // Sonderzeichen
+                             "0123456789" + // Zahlen
+                             "abcdefghijklmnopqrstuvwxyz" + // Alphabet klein
+                             "ABCDEFGHIJKLMNOPQRSTUVWXYZ"; // Alphabet groß
+            StringBuilder Text = new StringBuilder(); // String Zusammensetzung
+            Random Zufall = new Random(); // Zufalls Generator
+            for (int i = 0; i < Convert.ToInt32(textBox1.Text); i++) // For Schleife
+            {
+                int Stelle = Zufall.Next(Zeichen.Length);
+                Text.Append(Zeichen[Stelle]); // Zeichen am Ende des Strings anfügen
+            }
+            richTextBox1.Text = Text.ToString(); // Ausgabe
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            richTextBox1.Clear(); // Leeren
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            Application.Exit(); // Beenden
+        }
+    }
+}
